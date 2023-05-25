b7f4d63bff29ebd1bdae3e763a9d2a38bb859c73
Shreyash <shreyashyaduvanshi@gmail.com>
2023-05-25 22:20:43 +0530

    feat: create leetcode_48_Rotate_Image.py

---
+++ leetcode_48_Rotate_Image.py
@@ -0,0 +1,7 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        matrix.reverse()
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                if i < j:
+                    matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
